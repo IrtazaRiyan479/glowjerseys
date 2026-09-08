0a52bf42539ef1e768aeb9d58e1e3ba0d4b3fa25
Irtaza Riyan <martinirtaza@gmail.com>
2026-09-08 11:02:45 +0500

    fix: update activeIntensity in NeonText component to match Model_14 specifications

--- components/3d/Model/NeonText.tsx
+++ components/3d/Model/NeonText.tsx
@@ -117,7 +117,7 @@ export default function NeonText({
   const materialRef = useRef<THREE.ShaderMaterial>(null);
 
   // Synced exactly to Model_14.tsx outline intensity
-  const activeIntensity = 4.0; 
+  const activeIntensity = 2.2; 
 
   useFrame((_, delta) => {
     if (materialRef.current) {
